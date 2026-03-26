03cbd12e4a03efbbfe242afef3d7c04e23b558a4
Diego Maidana <die.maidana1@gmail.com>
2026-03-26 00:15:12 -0300

    first commit

---
+++ src/test/java/com/diedev/hexagonal_demo/HexagonalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diedev.hexagonal_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
