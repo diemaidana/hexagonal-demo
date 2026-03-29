7412bbf9e8988dcaba9d79c2b5227fc1800a3274
Diego Maidana <die.maidana1@gmail.com>
2026-03-28 23:03:34 -0300

    fix: adds constructor with all arguments.

--- src/main/java/com/diedev/hexagonal_demo/domain/model/Product.java
+++ src/main/java/com/diedev/hexagonal_demo/domain/model/Product.java
@@ -16,6 +16,13 @@ public class Product {
         this.status = ProductStatus.INACTIVE;
     }
 
+    public Product(UUID id, String name, BigDecimal price, ProductStatus status) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.status = status;
+    }
+
     public void activateProduct() {
         if (this.price == null || this.price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("Regla de negocio violada: No se puede activar un producto con precio negativo o nulo.");
